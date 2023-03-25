9952223085990d737b850e6f7bcac8a11745913e
xandjiji <xandjiji@gmail.com>
2023-03-25 14:58:28 -0300

    feature: setuping Sanity

---
+++ sanity.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+
+export default defineConfig({
+  name: "xandjiji_CMS",
+  title: "xandjiji_CMS",
+
+  projectId,
+  dataset,
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+});
