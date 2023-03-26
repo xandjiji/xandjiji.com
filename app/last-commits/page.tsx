212b9beb51bf26b06ad4c7eca94b3fa25e7c9c8c
xandjiji <xandjiji@gmail.com>
2023-03-26 17:16:14 -0300

    refactor: removing dependency

--- app/last-commits/page.tsx
+++ app/last-commits/page.tsx
@@ -1,7 +1,3 @@
-import { Octokit } from "octokit";
-
-const githubClient = new Octokit();
-
 type Commit = {
   sha: string;
   author: { email: string; name: string };
@@ -11,21 +7,39 @@ type Commit = {
 };
 
 export default async function LastCommits() {
-  const result = await githubClient.request(
-    "GET /users/{username}/events/public",
-    { username: "xandjiji" }
+  const lastCommits: Commit[] = await fetch(
+    "https://api.github.com/users/xandjiji/events"
+  )
+    .then((res) => res.json())
+    .then((data: any[]) =>
+      data
+        .filter(({ type }) => type === "PushEvent")
+        .map(({ payload, created_at }) =>
+          payload.commits.map((commit: Commit) => ({
+            ...commit,
+            createdAt: created_at ? new Date(created_at) : undefined,
+          }))
+        )
+        .flat()
+        .filter(({ author }) => author.email === "xandjiji@gmail.com")
+        .map((commit: Commit) => {
+          const [, , , , owner, repo] = commit.url.split("/");
+          return {
+            ...commit,
+            url: `https://github.com/${owner}/${repo}/commit/${commit.sha}`,
+          };
+        })
+    );
+
+  return (
+    <main>
+      <ul>
+        {lastCommits.map(({ sha, author, message, createdAt, url }) => (
+          <li key={`${sha}-${createdAt}`}>
+            {author.name} - <a href={url}>{sha}</a>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
-
-  const lastCommits = result.data
-    .filter(({ type }) => type === "PushEvent")
-    .map(({ payload, created_at }) =>
-      (payload as typeof payload & { commits: Commit[] }).commits.map(
-        (commit) =>
-          created_at ? { ...commit, createdAt: new Date(created_at) } : commit
-      )
-    )
-    .flat()
-    .filter(({ author }) => author.email === "xandjiji@gmail.com");
-
-  return <main>This is the last commits</main>;
 }
