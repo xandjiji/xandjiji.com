35af9918488460cd3c6ac154a319d664e8bc66d3
xandjiji <xandjiji@gmail.com>
2023-03-26 12:37:00 -0300

    configuring

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>xandjij</title>
+      <meta content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
